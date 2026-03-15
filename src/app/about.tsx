c6c23734e60285935ec26feea8160acb5bb02429
Inba <inbavarunans@gmail.com>
2026-03-15 22:04:08 +0530

    project-setup

---
+++ src/app/about.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Image } from 'expo-image'
+
+const about = () => {
+  return (
+    <View>
+      <Text>about</Text>
+    </View>
+  )
+}
+
+export default about
