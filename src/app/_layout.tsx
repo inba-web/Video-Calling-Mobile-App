784f479ebc7abea544ea0a18fc9952cc4cd2fdb0
Inba <inbavarunans@gmail.com>
2026-03-27 16:34:42 +0530

    implemented stream-io

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "../../global.css";
 import * as WebBrowser from "expo-web-browser";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -35,10 +36,12 @@ Sentry.init({
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <GestureHandlerRootView className="flex-1">
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="auth" />
         <Stack.Screen name="(tabs)" />
       </Stack>
+      </GestureHandlerRootView>
     </ClerkProvider>
   );
 }
