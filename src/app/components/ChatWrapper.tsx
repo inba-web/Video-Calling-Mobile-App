87c8713ab1af2b85515b36c7307f5fab5962d25a
Inba <inbavarunans@gmail.com>
2026-03-31 14:46:35 +0530

    wrapped stream users

---
+++ src/app/components/ChatWrapper.tsx
@@ -0,0 +1,18 @@
+
+const STREAM_API_KEY =  process.env.EXPO_PUBLIC_STREAM_API_KEY
+
+const syncUserToStream = 
+
+
+const Chatclient = () => {
+    return null;
+}
+
+const ChatWrapper = ({children}:{children: React.ReactNode}) => {
+  return 
+    <ChatClient>
+        {children} 
+    </ChatClient>
+}
+
+export default ChatWrapper
