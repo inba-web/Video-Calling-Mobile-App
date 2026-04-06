9608bf2f26bd7b0a08d6e9a8f74b08ebb43314a6
Inba <inbavarunans@gmail.com>
2026-04-06 22:15:57 +0530

    updated chatwrapper

--- src/app/components/ChatWrapper.tsx
+++ src/app/components/ChatWrapper.tsx
@@ -2,8 +2,13 @@ import { useUser } from "@clerk/clerk-expo";
 import type {UserResource} from "@clerk/types"
 import { useEffect, useRef } from "react";
 import { FullScreenLoading } from "./FullScreenLoading";
+import { Chat, OverlayProvider, useCreateChatClient } from "stream-chat-expo";
 
-const STREAM_API_KEY =  process.env.EXPO_PUBLIC_STREAM_API_KEY
+const STREAM_API_KEY =  process.env.EXPO_PUBLIC_STREAM_API_KEY;
+
+if (!STREAM_API_KEY) {
+  throw new Error("STREAM API KEY is missing. Check .env configuration.");
+}
 
 const syncUserToStream = async (user: UserResource) => {
     try {
@@ -31,8 +36,40 @@ const Chatclient = ({children,user}: {children: React.ReactNode,user:UserResourc
         }
     },[user])
 
-    return <></>
+    const tokenProvider = async () => {
+        const response = await fetch("/api/token", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({userId: user.id}),
+        });
+        const data = await response.json();
+        return data.token;
+    }
+
+    const chatClient = useCreateChatClient({
+        apiKey:STREAM_API_KEY,
+        userData: {
+            id: user.id,
+            name: user.fullName ?? user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+            image: user.imageUrl,
+        },
+        tokenOrProvider: tokenProvider
+    })
+
+    if(!chatClient){
+        return <FullScreenLoading message="Connecting to chat..." /> 
+    }
+
+    return(
+        <OverlayProvider>
+            <Chat client={chatClient} style={{}}>
+                {children}
+            </Chat>
+        </OverlayProvider>
+    )
 }
+
+
 
 const ChatWrapper = ({children}:{children: React.ReactNode}) => {
     const {user, isLoaded } = useUser();
