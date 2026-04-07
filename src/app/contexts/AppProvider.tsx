b859bf5c532434d4cef63b2801982803dd0f0119
Inba <inbavarunans@gmail.com>
2026-04-07 13:03:02 +0530

    updated get-stream-io

--- src/app/contexts/AppProvider.tsx
+++ src/app/contexts/AppProvider.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import type {Channel, LocalMessage} from "stream-chat";
+import ChatWrapper from "../components/ChatWrapper";
 
 type AppContextType = {
   channel: null | Channel;
@@ -18,7 +19,7 @@ export const AppContext = React.createContext<AppContextType>({
 export const AppProvider = ({ children }: {children: React.ReactNode}) => {
   const [channel, setChannel] = useState<Channel | null>(null);
   const [thread, setThread] = useState<LocalMessage | null>(null);
-
+ChatWrapper
   return (
     <AppContext.Provider value={{ channel, setChannel, thread, setThread }}>
       {children}
